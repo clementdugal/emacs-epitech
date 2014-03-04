4d988ec6d995165a38b220d79ae91abb3e686edc
dugal_c <dugal_c@epitech.eu>
2014-03-04 02:40:47 +0100

    updated README and install.sh

--- install.sh
+++ install.sh
@@ -5,10 +5,11 @@
 ## Login   <dugal_c@epitech.net>
 ## 
 ## Started on  Mon Mar  3 18:28:48 2014 Clement DUGAL
-## Last update Mon Mar  3 18:33:09 2014 Clement DUGAL
+## Last update Tue Mar  4 02:40:44 2014 Clement DUGAL
 ##
 
 echo "Copying files..."
 cp files/.emacs ~/
 cp -r files/.emacs.d/ ~/
+cp -r files/site-lisp/ /usr/share/emacs/
 echo "Done!"
